bb183f7fc8ac2414e709dfb2cacfe105795ad8da
Tim Park <timfpark@gmail.com>
2013-02-19 12:48:56 -0800

    handle null states, add timestamp to messages

--- app/controllers/messages.js
+++ app/controllers/messages.js
@@ -1,9 +1,7 @@
 App.MessagesController = Ember.ArrayController.extend({
-	messageBody: "test",
+	messageBody: "",
 
 	add: function() {
-		console.log('add pressed: ' + this.get('messageBody'));
-
         var transaction = App.store.transaction();
         var message = transaction.createRecord(App.Message, { body: this.get('messageBody') });
         transaction.commit();
--- app/models/message.js
+++ app/models/message.js
@@ -1,4 +1,15 @@
 App.Message = DS.Model.extend({
   timestamp: DS.attr('date'),
-  body: DS.attr('string')
+  body: DS.attr('string'),
+
+  // example of a property - there are obviously more globalized ways of doing this.
+  timestampString: function() {
+  	var timestamp = this.get('timestamp');
+  	if (!timestamp) return "";
+
+  	var month = timestamp.getMonth() + 1;
+
+  	return "(" + timestamp.getHours() + ":" + timestamp.getMinutes() + " " + timestamp.getDate() + "/" + month + "/" + timestamp.getFullYear() + ")";
+  }.property('timestamp')
+
 });
