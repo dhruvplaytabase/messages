ae68c0d9b30302d008621af704e1766116c8e2c1
Tim Park <timfpark@gmail.com>
2013-03-06 14:41:36 +0000

    take out debug messages

--- server.js
+++ server.js
@@ -11,17 +11,12 @@ app.use(express.bodyParser());
 /* serve static content out of 'build' directory */ 
 app.use(express.static(__dirname + '/build'));
 
+/* routes to hook up messages resource endpoints. */
 app.post('/messages', controllers.messages.create);
 app.get('/messages', controllers.messages.index);
 app.get('/messages/:id', controllers.messages.show);
 
-console.log("mongodb connection string: " + config.mongodb_connection_string);
 mongoose.connect(config.mongodb_connection_string);
-
-var db = mongoose.connection;
-db.once('open', function callback() {
-	console.log("mongodb connection established");
-});
 
 port = process.env.PORT || config.http_port || 3030;
 
