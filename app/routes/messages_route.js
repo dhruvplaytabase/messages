5a3084071be00f6e8c02a22df970556e9e778bc0
Tim Park <timfpark@gmail.com>
2013-08-30 13:06:57 -0700

    remove ember.js application (moved to messages-frontend project)

--- app/routes/messages_route.js
+++
@@ -1,8 +0,0 @@
-App.MessagesRoute = Ember.Route.extend({
-
-  /* Models that we should use for this route. */
-  model: function() {
-    return App.Message.find();
-  }
-
-});
--- app/routes/router.js
+++
@@ -1,8 +0,0 @@
-require('app/routes/messages_route');
-
-/* Define a resource called messages off of root of application */
-
-App.Router.map(function() {
-  this.route("about");
-  this.resource("messages");
-});
